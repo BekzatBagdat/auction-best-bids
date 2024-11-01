c8e6d8b4756266a822ad7983992466887d6d14b8
Bekzat Bagdat <paintdex12@gmail.com>
2024-11-01 03:29:28 +0100

    api.js

---
+++ src/js/constants/api.js
@@ -0,0 +1,8 @@
+//Listings
+const BASE_URL = 'https://v2.api.noroff.dev/';
+export const LISTINGS_URL = `${BASE_URL}auction/listings/`;
+//Profile
+export const PROFILES_URL = `${BASE_URL}/auction/profiles`;
+export const REGISTER_URL = `${BASE_URL}/auction/profiles`;
+// Params
+export const searchListingParams = `search?q=`;
